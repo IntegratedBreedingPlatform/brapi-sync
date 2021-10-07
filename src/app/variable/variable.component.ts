bcead2acae0a71cd7d97f84de4c9f71a2f5c97eb
Aldrin Batac <aldrin.batac@leafnode.io>
2021-10-07 14:01:17 +0800

    Bug fixes.
    IBP-4303

--- src/app/variable/variable.component.ts
+++ src/app/variable/variable.component.ts
@@ -187,7 +187,19 @@ export class VariableComponent implements OnInit {
     const targetVariable = this.variablesMap[sourceVariable.observationVariableName];
     // Source variable should have a match in the target server
     // Source and target variable should have the same datatype.
-    return targetVariable && targetVariable.scale.dataType === sourceVariable.scale.dataType;
+    return targetVariable && targetVariable.scale.dataType === sourceVariable.scale.dataType && this.isValidCategoricalValues(sourceVariable, targetVariable);
+  }
+
+  isValidCategoricalValues(sourceVariable: any, targetVariable: any) {
+    // Check if the source and target variables cagetorical valid values are equal
+    if (targetVariable.scale.dataType === sourceVariable.scale.dataType && targetVariable.scale.dataType === 'Nominal') {
+        const sortArray = (array: any[]) => array.sort((a, b) => (a.value > b.value) ? 1 : -1);
+        const objectsEqual = (o1:any, o2:any) => Object.keys(o1).length === Object.keys(o2).length && Object.keys(o1).every(p => o1[p] === o2[p]);
+        const arraysEqual = (a1: any[], a2: any[]) => a1.length === a2.length && a1.every((o: any, idx: any) => objectsEqual(o, a2[idx]));
+        return arraysEqual(sortArray(sourceVariable.scale.validValues.categories), sortArray(targetVariable.scale.validValues.categories));
+    } else {
+      return true;
+    }
   }
 
   canProceed() : boolean {
@@ -200,3 +212,7 @@ export class VariableComponent implements OnInit {
   }
 
 }
+function hash(a: any) {
+  throw new Error('Function not implemented.');
+}
+
