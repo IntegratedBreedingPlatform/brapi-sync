a2b20412fee4063d24f89f9ca68fc858716adeda
Aldrin Batac <aldrin.batac@leafnode.io>
2021-10-07 14:15:43 +0800

    Add faExclamationTriangle icon
    IBP-4303

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { AuthInterceptor } from './auth.interceptor';
 import { EntitySelectorComponent } from './entity-selector/entity-selector.component';
 import { CollapsibleComponent } from './shared/collapsible/collapsible.component';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCaretDown, faCaretRight, faCheck, faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown, faCaretRight, faCheck, faExclamationTriangle, faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { TrialComponent } from './trial/trial.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { NgxJsonViewerModule } from 'ngx-json-viewer';
@@ -70,7 +70,7 @@ export class AppModule {
   constructor(faIconLibrary: FaIconLibrary) {
     // Add an icon to the library for convenient access in other components
     // Only add specific icons explicitly to avoid loading all icons to the bundle.
-    faIconLibrary.addIcons(faCaretDown, faCaretRight, faPlus, faCheck, faTimes);
+    faIconLibrary.addIcons(faCaretDown, faCaretRight, faPlus, faCheck, faTimes, faExclamationTriangle);
   }
 
 }
